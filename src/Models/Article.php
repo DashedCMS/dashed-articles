0b972200f6b9dd8af3b4040b22b1b9756c4d57d7
Robin van Maasakker <robin@qubiqx.com>
2024-11-21 10:10:34 +0100

    Update

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -15,7 +15,7 @@ use Dashed\DashedCore\Models\Concerns\IsVisitable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Dashed\DashedCore\Models\Concerns\HasCustomBlocks;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Dashed\LaravelLocalization\Facades\LaravelLocalization;
 
 class Article extends Model
 {
--- src/Models/ArticleCategory.php
+++ src/Models/ArticleCategory.php
@@ -13,7 +13,7 @@ use Dashed\DashedCore\Models\Concerns\IsVisitable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Dashed\DashedCore\Models\Concerns\HasCustomBlocks;
-use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Dashed\LaravelLocalization\Facades\LaravelLocalization;
 
 class ArticleCategory extends Model
 {
