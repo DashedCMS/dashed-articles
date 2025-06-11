5b2bc420701456f28ccecfb784c3ff400524525e
Robin van Maasakker <robin@qubiqx.com>
2025-06-11 18:13:52 +0200

    Update

--- src/Models/Article.php
+++ src/Models/Article.php
@@ -52,7 +52,7 @@ class Article extends Model
     {
         if ($this->category) {
             $article = $this->category->articles()->thisSite()->publicShowable()->where('id', '>', $this->id)->orderBy('id', 'ASC')->first();
-            if (! $article && $useFirstArticleIfIsLast) {
+            if (!$article && $useFirstArticleIfIsLast) {
                 $article = $this->category->articles()->thisSite()->publicShowable()->orderBy('id', 'ASC')->first();
                 if ($article->id == $this->id) {
                     $article = null;
@@ -60,7 +60,7 @@ class Article extends Model
             }
         } else {
             $article = Article::thisSite()->publicShowable()->where('id', '>', $this->id)->orderBy('id', 'ASC')->first();
-            if (! $article && $useFirstArticleIfIsLast) {
+            if (!$article && $useFirstArticleIfIsLast) {
                 $article = Article::thisSite()->publicShowable()->orderBy('id', 'ASC')->first();
                 if ($article->id == $this->id) {
                     $article = null;
@@ -102,18 +102,18 @@ class Article extends Model
         $overviewPage = self::getOverviewPage();
         $article = self::resolveArticle($lastSlugPart, $slugComponents);
 
-        if (! $article) {
+        if (!$article) {
             return;
         }
 
-        if (! self::isValidSlugStructure($article, $overviewPage, $slugComponents, $secondLastSlugPart)) {
+        if (!self::isValidSlugStructure($article, $overviewPage, $slugComponents, $secondLastSlugPart)) {
             return;
         }
 
         if ($overviewPage) {
 
             $page = self::getPageIfExists($overviewPage, $slugComponents[0]);
-            if (! $page) {
+            if (!$page) {
                 return;
             }
         }
@@ -133,8 +133,8 @@ class Article extends Model
         $useCategoryInUrl = Customsetting::get('article_use_category_in_url', null, false);
         $hasOverviewPage = $overviewPage && $overviewPage->id;
 
-        return (! $useCategoryInUrl && count($slugComponents) === ($overviewPage ? 2 : 1))
-            || (! $article->category && count($slugComponents) === ($overviewPage ? 2 : 1))
+        return (!$useCategoryInUrl && count($slugComponents) === ($overviewPage ? 2 : 1))
+            || (!$article->category && count($slugComponents) === ($overviewPage ? 2 : 1))
             || ($useCategoryInUrl && $article->category && $article->category->slug === $secondLastSlugPart && count($slugComponents) > ($overviewPage ? 2 : 1));
     }
 
@@ -149,7 +149,7 @@ class Article extends Model
 
     private static function renderArticleView($article, $page)
     {
-        if (! View::exists(env('SITE_THEME', 'dashed') . '.articles.show')) {
+        if (!View::exists(env('SITE_THEME', 'dashed') . '.articles.show')) {
             return 'pageNotFound';
         }
 
@@ -303,7 +303,7 @@ class Article extends Model
     {
         $originalLocale = app()->getLocale();
 
-        if (! $activeLocale) {
+        if (!$activeLocale) {
             $activeLocale = $originalLocale;
         }
 
@@ -320,22 +320,28 @@ class Article extends Model
         }
 
         if (Customsetting::get('article_use_category_in_url') && $this->category) {
+            $categoriesToAdd = [];
             $parentCategory = $this->category;
             while ($parentCategory) {
-                $url = "/{$parentCategory->getTranslation('slug', $activeLocale)}" . $url;
+                $categoriesToAdd[] = $parentCategory->getTranslation('slug', $activeLocale);
                 $parentCategory = $parentCategory->parent;
+            }
+            $categoriesToAdd = array_reverse($categoriesToAdd);
+            foreach($categoriesToAdd as $categorySlug) {
+                $url .= "{$categorySlug}/";
             }
         }
 
         $url .= $this->getTranslation('slug', $activeLocale);
 
-        if (! str($url)->startsWith('/')) {
+        if (!str($url)->startsWith('/')) {
             $url = '/' . $url;
         }
-        if ($activeLocale != Locales::getFirstLocale()['id'] && ! str($url)->startsWith("/{$activeLocale}")) {
+        if ($activeLocale != Locales::getFirstLocale()['id'] && !str($url)->startsWith("/{$activeLocale}")) {
             $url = '/' . $activeLocale . $url;
         }
 
+//        dd($url);
         return $native ? $url : url($url);
     }
 
